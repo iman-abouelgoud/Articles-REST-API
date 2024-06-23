d7fa09eedbd38030d60f280c645575f8fbf117f1
eman <eman.abualgoud@gmail.com>
2024-06-23 20:45:33 +0400

    add project

---
+++ database/factories/ArticleFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+use App\Models\User;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Article>
+ */
+class ArticleFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $title = $this->faker->sentence();
+
+        return [
+            'title' => $this->faker->sentence(),
+            'slug' => Str::slug($title),
+            'body' => $this->faker->paragraph(),
+            'user_id' => $attribute['user_id'] ?? User::factory(),
+        ];
+    }
+}
